ac10827fd2aa8b972ca6ed4b10ef9ebf893b1c95
Victor costa <costavictor771@gmail.com>
2022-04-05 14:42:36 -0300

    deu ruim papaikkk

--- src/app/model/UserPostagem.ts
+++ src/app/model/UserPostagem.ts
@@ -3,8 +3,8 @@ import { UserTema } from "./UserTema";
 
 export class UserPostagem{
     public id: number;
-    public title: String;
-    public description: String;
+    public title: string;
+    public description: string;
     public date: Date;
 
     public UserModel: UserModel
